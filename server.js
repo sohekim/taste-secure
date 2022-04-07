5fd663e7c6d2be5bf7885dd36bd6265bcb794d0b
Sohee Kim <kim66s@mtholyoke.edu>
2022-04-07 15:46:01 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolver");
+const mongoose = require('mongoose');
+const url = require('./key');
+
+async function startApolloServer(typeDefs, resolvers) {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+    console.log(url);
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Mongoose connected ...');
+
+    await new Promise((resolve) => app.listen({ port: 4040 }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4040${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers);
