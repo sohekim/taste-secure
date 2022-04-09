565de16b7cd3fc29f9bd3b912084ba31ad2cb7ba
Sohee Kim <kim66s@mtholyoke.edu>
2022-04-09 01:03:24 -0400

    add recipe resolver

--- Schema/resolver.js
+++ Schema/resolver.js
@@ -1,4 +1,5 @@
 const Post = require("../models/Post.model");
+const Recipe = require("../models/Recipe.model");
 
 const resolvers = {
     Query: {
@@ -12,10 +13,49 @@ const resolvers = {
         getPost: async(_, { id }) => {
             return await Post.findById(id);
         },
+
+        getAllRecipes: async() => {
+            return await Recipe.find();
+        },
+        getRecipe: async(_, { id }) => {
+            return await Recipe.findById(id);
+        },
     },
 
     Mutation: {
-        createPost: async(parent, args) => {
+        createRecipe: async(_, args) => {
+            const {
+                name,
+                description,
+                price_per_serving,
+                minutes,
+                main_ingredients,
+                instruction,
+            } = args.recipe;
+
+            const newRecipe = {
+                name: name,
+                description: description,
+                price_per_serving: price_per_serving,
+                minutes: minutes,
+                main_ingredients: main_ingredients,
+                instruction: instruction,
+            };
+            const recipe = await new Recipe(newRecipe).save();
+            return recipe;
+        },
+
+        deleteRecipe: async(_, { id }) => {
+            try {
+                await Recipe.findByIdAndDelete(id);
+                return true;
+            } catch (err) {
+                console.log(err);
+                return false;
+            }
+        },
+
+        createPost: async(_, args) => {
             const { title, description } = args.post;
             const newPost = {
                 title: title,
@@ -38,7 +78,9 @@ const resolvers = {
                 description: description,
             };
 
-            const updatedpost = await Post.findByIdAndUpdate(id, newPost, { new: true });
+            const updatedpost = await Post.findByIdAndUpdate(id, newPost, {
+                new: true,
+            });
             return updatedpost;
         },
     },
