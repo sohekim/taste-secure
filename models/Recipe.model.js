849ccb6ab8bdeac3b4bc7477f49b5a6e492e788d
Sohee Kim <kim66s@mtholyoke.edu>
2022-04-08 22:38:14 -0400

    add recipe model

---
+++ models/Recipe.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const recipeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price_per_serving: {
+        type: Number,
+        required: true,
+    },
+    minutes: {
+        type: Number,
+        required: true,
+    },
+    // nutritions: {
+    //     type: [{ type: String, val: Number }],
+    //     required: true,
+    // },
+    // calories,
+    main_ingredients: {
+        type: [String],
+        required: true,
+    },
+    instruction: {
+        type: [String],
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+module.exports = Recipe = mongoose.model("recipe", recipeSchema);
