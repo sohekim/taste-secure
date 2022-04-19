4b1092ac600076fe2b4f515ff09e385dd7e4d3b0
Sohee Kim <kim66s@mtholyoke.edu>
2022-04-19 14:44:57 -0400

    add prod db

--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const { ApolloServer } = require("apollo-server-express");
 const typeDefs = require("./Schema/typeDefs");
 const resolvers = require("./Schema/resolver");
 const mongoose = require('mongoose');
-const url = require('./key');
+const { devURL, prodURL } = require('./key');
 
 async function startApolloServer(typeDefs, resolvers) {
+    let PORT, baseURL, dbURL;
+
+    if (process.env.Node_ENV === "prod") {
+        PORT = process.env.PORT || 31600;
+        baseURL = "http://cs-vm-04.cs.mtholyoke.edu";
+        dbURL = prodURL;
+    } else {
+        PORT = 4040;
+        baseURL = "http://localhost";
+        dbURL = devURL;
+    }
+    console.log("Running environment in " + process.env.NODE_ENV);
+
     const app = express();
     const server = new ApolloServer({
         typeDefs,
@@ -14,24 +27,11 @@ async function startApolloServer(typeDefs, resolvers) {
 
     await server.start();
     server.applyMiddleware({ app });
-    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Mongoose connected ...');
 
-    let PORT;
-    let baseUrl;
-
-    console.log("Running environment in " + process.env.NODE_ENV);
-
-    if (process.env.Node_ENV === "prod") {
-        PORT = process.env.PORT || 31600;
-        baseUrl = "http://cs-vm-04.cs.mtholyoke.edu";
-    } else {
-        PORT = 4040;
-        baseUrl = "http://localhost";
-    }
-
     await new Promise((resolve) => app.listen({ port: PORT }, resolve));
-    console.log(`🚀 Server ready at ${baseUrl}:${PORT}${server.graphqlPath}`);
+    console.log(`🚀 Server ready at ${baseURL}:${PORT}${server.graphqlPath}`);
 }
 
 startApolloServer(typeDefs, resolvers);
